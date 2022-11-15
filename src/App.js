f490136bf815ac210972082f056fe967e68b558b
Andrew Park <andrewchanwonpark@gmail.com>
2022-11-15 08:19:19 -0800

    added zillow api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,61 @@
 import React from "react";
+import axios from "axios";
+import { useState } from "react";
+
 import NavBar from "./components/navbar/NavBar";
 import Hero from "./components/hero/Hero";
 import CtaSection from "./components/ctaSection/CtaSection";
 import Footer from "./components/footer/Footer";
 
 function App() {
+  const [listings, setListings] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [sortOrder, setSortOrder] = useState(2);
+
+  const length = listings.length;
+  console.log(length + ' this is the length');
+
+
+  const host = "zillow-com1.p.rapidapi.com";
+  const key = "fee072a1a6msh496832e5776cc2dp1e34a7jsn6c609534f78d";
+
+  const fetchListings = (query) => {
+    setLoading(true);
+    var options = {
+      method: "GET",
+      url: `https://zillow-com1.p.rapidapi.com/propertyExtendedSearch?location=${query}&home_type=Houses`,
+      // params: {location: 'plano, tx', page: '1', home_type: 'Houses'},
+      headers: {
+        "x-rapidapi-host": host,
+        "x-rapidapi-key": key,
+      },
+    };
+
+    axios
+      .request(options)
+      .then((response) => {
+        let props = response.data.props;
+
+        props = props.sort((a, b) => {
+          //  console.log('A', a);
+          if (sortOrder === 1) {
+            return a.price > b.price ? 1 : -1;
+          } else {
+            return a.price < b.price ? 1 : -1;
+          }
+        });
+        setListings(props);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <>
       <NavBar />
-      <Hero />
+      <Hero fetchListings={fetchListings} listings={listings} loading={loading}/>
       <CtaSection />
       <Footer />
     </>
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,23 +1,46 @@
-import React from 'react'
-import {AiOutlineSearch} from 'react-icons/ai';
+import React, { useState } from "react";
+import { AiOutlineSearch } from "react-icons/ai";
 
+import "./Hero.css";
 
-import './Hero.css';
+const Hero = ({ fetchListings, listings, loading }) => {
+  const length = listings.length;
 
+  const [query, setQuery] = useState("");
 
-const hero = () => {
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (query === "") {
+      alert("Please enter something");
+    } else {
+      fetchListings(query);
+      setQuery("");
+    }
+  };
+
+  const onChange = (e) => {
+    setQuery(e.target.value);
+  };
+
   return (
-    <div className='hero'>
-        <div className='content'>
-            <h1> Find homes first.</h1>
-            <h1> Tour homes fast.</h1>
-            <form className='search'>
-                    <input type='text' placeholder='City, Address, School, Agent, ZIP' />
-                    <button type='submit'><AiOutlineSearch className='icon'/></button>
-            </form>
-        </div>
+    <div className="hero">
+      <div className="content">
+        <h1> Find homes first.</h1>
+        <h1> Tour homes fast.</h1>
+        <form className="search" onSubmit={onSubmit}>
+          <input
+            value={query}
+            onChange={onChange}
+            type="text"
+            placeholder="City, Address, School, Agent, ZIP"
+          />
+          <button type="submit">
+            <AiOutlineSearch className="icon" />
+          </button>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default hero
+export default Hero;
