c2a0194def9cef0b5773379065f5d0cf5488f836
Andrew Park <andrewchanwonpark@gmail.com>
2022-11-14 17:29:45 -0800

    added hero section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from "react";
+import NavBar from "./components/NavBar";
+import Hero from "./components/hero";
 
 function App() {
   return (
-    <div>
-      <h1> Coding </h1>
-    </div>
+    <>
+      <NavBar />
+      <Hero />
+    </>
   );
 }
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { HiOutlineMenuAlt4 } from "react-icons/hi";
+import { FaRegTimesCircle } from "react-icons/fa";
+import BluefinLogo from "../assets/bluefin-logo.png";
+
+import "./Navbar.css";
+
+const NavBar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => {
+    setClick(!click);
+    console.log(click);
+  };
+
+  return (
+    <div className="navbar">
+      <div className="container">
+        <img alt="bluefin-logo" src={BluefinLogo}></img>
+        <div className="icon" onClick={handleClick}>
+          {click ? <FaRegTimesCircle /> : <HiOutlineMenuAlt4 />}
+        </div>
+        <div className={click ? "menu-container active" : "menu-container"}>
+          <ul className="nav-menu">
+            <a href="#"> Buy </a>
+            <a href="#"> Rent </a>
+            <a href="#"> Sell </a>
+            <a href="#"> Mortgage</a>
+            <a href="#"> Real Estate Agents </a>
+            <a href="#"> Feed </a>
+          </ul>
+          <button className="btn"> Log In </button>
+          <button className="btn"> Sign Up</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
